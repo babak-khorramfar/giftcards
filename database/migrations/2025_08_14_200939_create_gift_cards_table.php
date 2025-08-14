111a7e8450a485f85c0ead0ccd5262a648af8726
Babak Khorramfar <babak.khorramfar@gmail.com>
2025-08-14 23:44:37 +0330

    add main tables: users, gift_cards, orders, categories

---
+++ database/migrations/2025_08_14_200939_create_gift_cards_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gift_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code')->unique(); // کد گیفت کارت
+            $table->decimal('price', 12, 2);
+            $table->string('currency', 3)->default('USD');
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('gift_cards');
+    }
+};
